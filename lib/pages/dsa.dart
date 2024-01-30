eb9ae697fde74e396375871fbf2da0b41e8ef587
ilobosok <121606196+ivan-lab-pvls@users.noreply.github.com>
2024-01-30 15:55:16 +0100

    fix

--- lib/pages/dsa.dart
+++ lib/pages/dsa.dart
@@ -12,6 +12,9 @@ class RdXX extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+      ),
       body: SafeArea(
         bottom: false,
         child: InAppWebView(
